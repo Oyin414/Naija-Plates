d286bfe56bc6f1261701878bc2ae8c4782c66293
Oyin414 <kadirioyin88@gmail.com>
2026-06-13 04:14:42 +0100

    refactor: simplify prep time filter

--- src/app/recipe.js
+++ src/app/recipe.js
@@ -13,25 +13,8 @@ function searchName(name, array) {
 function maxPrepTime(number, array) {
   const prepArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (number === 15) {
-      if (array[i].prepTime <= 15) {
-        prepArray.push(array[i]);
-      }
-    }
-    if (number === 30) {
-      if (array[i].prepTime <= 30) {
-        prepArray.push(array[i]);
-      }
-    }
-    if (number === 45) {
-      if (array[i].prepTime <= 45) {
-        prepArray.push(array[i]);
-      }
-    }
-    if (number === 60) {
-      if (array[i].prepTime <= 60) {
-        prepArray.push(array[i]);
-      }
+    if (array[i].prepTime <= number) {
+      prepArray.push(array[i]);
     }
   }
   return prepArray;
