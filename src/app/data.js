d2cd3e51d3ab1ee403f4cb0519292e3457612cbb
Oyin414 <kadirioyin88@gmail.com>
2026-06-17 03:48:47 +0100

    feat: add complete Jollof Rice recipe

--- src/app/data.js
+++ src/app/data.js
@@ -1,30 +1,105 @@
+/*
+{
+id: 1,
+name: "Jollof Rice",
+shortDescription: "A rich tomato-based rice dish cooked with peppers, onions, and spices.",
+description:
+"Jollof Rice is a popular West African one-pot rice dish made with a tomato and pepper sauce, onions, stock, and seasoning. It is commonly served at parties, celebrations, and family meals, and can be enjoyed on its own or with sides such as plantain or salad and with protein like meat or chicken.",
+categories: ["lunch", "dinner", "vegetarian"],
+prepTime: 25,
+cookTime: 75,
+servings: 5,
+ingredients: [
+"3 cups long-grain rice ",
+"4 medium tomatoes",
+"2 red bell peppers",
+"1 scotch bonnet pepper",
+"1 large onion",
+"3 tablespoons tomato paste",
+"1/4 cup vegetable oil",
+"2 cups vegetable stock",
+"1 cup water",
+"2 tablespoon curry powder",
+"1 teaspoon dried thyme",
+"1 bay leaf",
+"1 teaspoon paprika",
+"6 maggi seasoning tablets",
+"Salt to taste"
+],
+instructions: [
+"Blend the tomatoes, red bell peppers, scotch bonnet, and half of the onion until smooth.",
+"finely chop the remaining onion"
+"Heat the vegetable oil in a large pot over medium heat.",
+"fry the onion in the oil until soft and fragrant.",
+"Add the tomato paste and cook for 10 minutes, stirring often so it does not burn.",
+"Pour in the blended pepper mixture.",
+"Set the heat to low and season with curry powder, thyme, bay leaf, paprika, seasoning tablets, and salt.",
+"Cook the pepper mixture on medium heat for 15-20 minutes or until the pepper mix thickens"
+"While that is cooking rinse the rice several times until the water runs mostly clear, then drain and set aside.",
+"Add the vegetable stock and water, then stir well.",
+"Add the drained rice and stir until the rice is coated in the sauce.",
+"Cover the pot tightly with foil covering the pot and cook on low heat until the rice is tender and the liquid has absorbed.",
+"Check every 10 minutes and  it should be ready in around 30-40 minutes but add a small splash of water if the rice needs more time to soften and the pot is dry.",
+"Once ready turn off the heat and add a table spoon of oil and mix it in then let the rice steam for 5 minutes"
+],
+image: placeholderImage,
+imageAlt: "A plate of Nigerian Jollof Rice",
+imageCredit: "Photo by keesha-s-kitchen fron Upsplash"
+}
+
+
+*/
+
 import placeHolderImage from "../images/placeholder.jpg";
+import jollofRiceImage from "../images/jollof-rice.jpg";
 export const recipes = [
   {
     id: 1,
     name: "Jollof Rice",
-    shortDescription: "A tomato-based Nigerian rice dish.",
+    shortDescription:
+      "A rich tomato-based rice dish cooked with peppers, onions, and spices.",
     description:
-      "Jollof Rice is a popular Nigerian rice dish cooked in a rich tomato and pepper sauce. It is often served with fried plantain, chicken, or salad.",
+      "Jollof Rice is a popular West African one-pot rice dish made with a tomato and pepper sauce, onions, stock, and seasoning. It is commonly served at parties, celebrations, and family meals, and can be enjoyed on its own or with sides such as plantain or salad and with protein like meat or chicken.",
     categories: ["lunch", "dinner", "vegetarian"],
-    prepTime: 20,
-    cookTime: 60,
+    prepTime: 25,
+    cookTime: 75,
     servings: 5,
     ingredients: [
-      "Rice",
-      "Tomatoes",
-      "Red bell peppers",
-      "Onion",
-      "Scotch bonnet",
+      "3 cups long-grain rice ",
+      "4 medium tomatoes",
+      "2 red bell peppers",
+      "1 scotch bonnet pepper",
+      "1 large onion",
+      "3 tablespoons tomato paste",
+      "1/4 cup vegetable oil",
+      "2 cups vegetable stock",
+      "1 cup water",
+      "2 tablespoon curry powder",
+      "1 teaspoon dried thyme",
+      "1 bay leaf",
+      "1 teaspoon paprika",
+      "6 maggi seasoning tablets",
+      "Salt to taste",
     ],
-    steps: [
-      "Blend the tomatoes, peppers, and onion.",
-      "Cook the pepper mixture with oil and seasoning.",
-      "Add washed rice and enough stock or water.",
-      "Cover and cook on low heat until the rice is soft.",
-      "Fluff the rice and serve warm.",
+    instructions: [
+      "Blend the tomatoes, red bell peppers, scotch bonnet, and half of the onion until smooth.",
+      "finely chop the remaining onion",
+      "Heat the vegetable oil in a large pot over medium heat.",
+      "fry the onion in the oil until soft and fragrant.",
+      "Add the tomato paste and cook for 10 minutes, stirring often so it does not burn.",
+      "Pour in the blended pepper mixture.",
+      "Set the heat to low and season with curry powder, thyme, bay leaf, paprika, seasoning tablets, and salt.",
+      "Cook the pepper mixture on medium heat for 15-20 minutes or until the pepper mix thickens",
+      "While that is cooking rinse the rice several times until the water runs mostly clear, then drain and set aside.",
+      "Add the vegetable stock and water, then stir well.",
+      "Add the drained rice and stir until the rice is coated in the sauce.",
+      "Cover the pot tightly with foil covering the pot and cook on low heat until the rice is tender and the liquid has absorbed.",
+      "Check every 10 minutes and  it should be ready in around 30-40 minutes but add a small splash of water if the rice needs more time to soften and the pot is dry.",
+      "Once ready turn off the heat and add a table spoon of oil and mix it in then let the rice steam for 5 minutes",
     ],
-    image: placeHolderImage,
+    image: jollofRiceImage,
+    imageAlt: "A plate of Nigerian Jollof Rice",
+    imageCredit: "Photo by keesha-s-kitchen fron Upsplash",
   },
   {
     id: 2,
